71a0cf9446afa78204c2906f54ff61dd970e3244
huss09sama-wq <huss09sama@gmail.com>
2026-06-06 03:06:25 +0300

    project finished....

--- src/pages/Addsnippet.jsx
+++ src/pages/Addsnippet.jsx
@@ -31,7 +31,7 @@ const AddSnippet = () => {
  const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-        const res = await fetch('http://localhost:3000/snippets', {
+        const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/snippets`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
